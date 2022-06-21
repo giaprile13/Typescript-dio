66b563a804ef0355caba5cb43b92197180695f66
giaprile13 <104396758+giaprile13@users.noreply.github.com>
2022-06-21 17:17:52 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,12 @@
+const employee = {
+  code : Number,
+  name : String
+  }
+  
+  
+  let employees = {};
+  employee.code = 10;
+  employee.name = "John";
+  
+  console.log(employee.code)
+  console.log(employee.name)
